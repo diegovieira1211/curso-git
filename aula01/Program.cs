44cc7153662e44ced434e242734258e780519a1e
Diego Vieira <diegovieira1211@outlook.com>
2018-09-27 10:49:29 -0300

    Projeto para calcular area triangulo

---
+++ aula01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            Triangulo X, Y;
+
+            double areaX, areaY;
+
+            X = new Triangulo();
+            Y = new Triangulo();
+
+
+            X.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            X.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Y.c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            areaX = X.area();
+
+            areaY = Y.area();
+
+            Console.WriteLine("AREA DO TRIANGULO X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("AREA DO TRIANGULO Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            if (areaX > areaY) {
+                Console.WriteLine("TRIANGULO DE MAIOR AREA: X");
+            }
+            else if (
+          areaY >
+          areaX) {
+                Console.WriteLine("TRIANGULO DE MAIOR AREA: Y");
+            }
+            else {
+                Console.WriteLine("AREAS IGUAIS");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ aula01/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace curso {
+    class Triangulo {
+
+        public double a, b, c;
+
+        public double area() {
+
+            double p = (a + b + c) / 2.0;
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+        }
+
+    }
+}
